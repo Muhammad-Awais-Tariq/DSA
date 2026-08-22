3765f44fba8e16d1f3efba11a25148a5d6088865
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-22 21:47:27 +0500

    added the helper function docstring

---
+++ Group Angram/group_anagram.py
@@ -0,0 +1,10 @@
+def check_anagram(s: str, t: str) -> bool:
+    """Check if two strings are anagrams.
+
+    Parameters:
+        s (str): The first string.
+        t (str): The second string.
+
+    Returns:
+        bool: True if both strings are anagrams, otherwise False.
+    """
