21fbe20c402b75ee8d295e656d27e91695429d67
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-22 21:52:43 +0500

    done the helper function

--- Group Angram/group_anagram.py
+++ Group Angram/group_anagram.py
@@ -8,3 +8,18 @@ def check_anagram(s: str, t: str) -> bool:
     Returns:
         bool: True if both strings are anagrams, otherwise False.
     """
+
+    if len(s) != len(t):
+        return False
+
+    alphabets = [0] * 26
+
+    for i in range(len(s)):
+        alphabets[ord(s[i])-ord("a")] +=1
+        alphabets[ord(t[i])-ord("a")] -=1
+
+    for i in range(len(alphabets)):
+        if alphabets[i] != 0:
+            return False
+
+    return True
