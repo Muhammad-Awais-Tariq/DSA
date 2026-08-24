1f3e799f5996ea615e5c487fe4ea3a44ab2794d3
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-24 12:49:05 +0500

    done my solution

--- Product of Array Except Self/product_of_array_except_self.py
+++ Product of Array Except Self/product_of_array_except_self.py
@@ -9,3 +9,18 @@ def productExceptSelf(nums: list) -> list:
         list: A list containing the product of all elements except
             the element at the corresponding index.
     """
+
+    total_size = len(nums)
+    final_array = [None] * total_size
+
+    for i in range(total_size):
+        product = 1
+        for j in range(total_size):
+            if i == j:
+                continue
+            product *= nums[j]
+        final_array[i] = product
+
+    return final_array
+
+print(productExceptSelf([-1,1,0,-3,3]))
