f3e29665cbb9cfc649a854b28564d8f21d4a8e76
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-24 12:43:52 +0500

    added the doc string

---
+++ Product of Array Except Self/product_of_array_except_self.py
@@ -0,0 +1,11 @@
+
+def productExceptSelf(nums: list) -> list:
+    """Return the product of all elements except the element at each index.
+
+    Parameters:
+        nums (list): The list of numbers.
+
+    Returns:
+        list: A list containing the product of all elements except
+            the element at the corresponding index.
+    """
