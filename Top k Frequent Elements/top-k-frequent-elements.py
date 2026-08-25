1427378bcf99bccd569eb0d86980b74db8c7495d
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-25 16:02:49 +0500

    added the docstring

---
+++ Top k Frequent Elements/top-k-frequent-elements.py
@@ -0,0 +1,13 @@
+
+# ------------------------------- My solution ------------------------------
+
+def topKFrequent(nums: list, k: int) -> list:
+    """Return the k elments that has highest frequency of elements in the nums.
+
+    Parameters:
+        nums (list): The list of the numbers.
+        k (int) : The total number of elements that we hv to return.
+    
+    Return:
+        list : The final list of elements.
+    """
