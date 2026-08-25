d4d07d75bcec02c5f41d092b92328adcdf864708
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-25 16:03:11 +0500

    solved the problem

--- Top k Frequent Elements/top-k-frequent-elements.py
+++ Top k Frequent Elements/top-k-frequent-elements.py
@@ -11,3 +11,20 @@ def topKFrequent(nums: list, k: int) -> list:
     Return:
         list : The final list of elements.
     """
+
+    hash_map = {}
+
+    for num in nums:
+        if num in hash_map:
+            hash_map[num] += 1
+        else:
+            hash_map[num] = 1
+
+    temp_answer = [(None , float('-inf'))] * k
+
+    for key , value in hash_map.items():
+        if value > temp_answer[0][1]:
+            temp_answer[0] = (key , value)
+            temp_answer = sorted(temp_answer , key= lambda x : x[1])
+
+    return [value[0] for value in temp_answer]
