0e84b83d9eff99c4fce1c6466d152aabd6f385ef
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-25 16:55:28 +0500

    done the solution

--- Top k Frequent Elements/top-k-frequent-elements.py
+++ Top k Frequent Elements/top-k-frequent-elements.py
@@ -1,30 +1,32 @@
 
+
+
 # ------------------------------- My solution ------------------------------
 
-def topKFrequent(nums: list, k: int) -> list:
-    """Return the k elments that has highest frequency of elements in the nums.
+# def topKFrequent(nums: list, k: int) -> list:
+#     """Return the k elments that has highest frequency of elements in the nums.
 
-    Parameters:
-        nums (list): The list of the numbers.
-        k (int) : The total number of elements that we hv to return.
+#     Parameters:
+#         nums (list): The list of the numbers.
+#         k (int) : The total number of elements that we hv to return.
     
-    Return:
-        list : The final list of elements.
-    """
+#     Return:
+#         list : The final list of elements.
+#     """
 
-    hash_map = {}
+#     hash_map = {}
 
-    for num in nums:
-        if num in hash_map:
-            hash_map[num] += 1
-        else:
-            hash_map[num] = 1
+#     for num in nums:
+#         if num in hash_map:
+#             hash_map[num] += 1
+#         else:
+#             hash_map[num] = 1
 
-    temp_answer = [(None , float('-inf'))] * k
+#     temp_answer = [(None , float('-inf'))] * k
 
-    for key , value in hash_map.items():
-        if value > temp_answer[0][1]:
-            temp_answer[0] = (key , value)
-            temp_answer = sorted(temp_answer , key= lambda x : x[1])
+#     for key , value in hash_map.items():
+#         if value > temp_answer[0][1]:
+#             temp_answer[0] = (key , value)
+#             temp_answer = sorted(temp_answer , key= lambda x : x[1])
 
-    return [value[0] for value in temp_answer]
+#     return [value[0] for value in temp_answer]
