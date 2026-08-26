f26c0de3305e5b7da98dedf107a71b2a56bd4496
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-26 13:44:48 +0500

    added the docstring

--- Roman To Integer/roman-to-integer.py
+++ Roman To Integer/roman-to-integer.py
@@ -1,1 +1,12 @@
 
+# ---------------------------- My solution --------------------------
+
+def romanToInt(s: str) -> int:
+    """Takes a roman interger and return its int value.
+
+    Parameters:
+        s (str): Roman integer.
+    
+    Returns:
+        int: Integer value.
+    """
