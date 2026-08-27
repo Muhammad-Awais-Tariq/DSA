d2c80cdabcd617e380840343bc2a0c4cd9e1567e
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-27 10:53:15 +0500

    added the docstring

---
+++ Verifying an Alien Dictionary/verifying-an-alien-dictionary.py
@@ -0,0 +1,12 @@
+
+def isAlienSorted(words: list, order: str) -> bool:
+    """Checks if the words list is sorted according to the order.
+
+    Parameters:
+        words (list): The words to check if they are sorted or not.
+        order (str): The order of the words according to which we hv to check.
+    
+    Return:
+        bool: True if they are sorted accordingly else False.
+    """
+    
