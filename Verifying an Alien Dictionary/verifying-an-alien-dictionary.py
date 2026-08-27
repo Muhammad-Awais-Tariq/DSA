8cb7b11c59981db0fd4a5365619a256e1742924e
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-27 11:43:41 +0500

    solved: verifying-an-alien-dictionary  [Hashmap]

--- Verifying an Alien Dictionary/verifying-an-alien-dictionary.py
+++ Verifying an Alien Dictionary/verifying-an-alien-dictionary.py
@@ -9,4 +9,28 @@ def isAlienSorted(words: list, order: str) -> bool:
     Return:
         bool: True if they are sorted accordingly else False.
     """
+
+    order_map = {}
+
+    for i , char in enumerate(order):
+        order_map[char] = i
     
+    for i in range(len(words) - 1):
+
+        for j in range(len(words[i])):
+
+            if j >= len(words[i+1]):
+                return False
+
+            if words[i][j] != words[i+1][j]:
+                current_letter = order_map[words[i][j]]
+                next_letter = order_map[words[i + 1][j]]
+
+                if current_letter > next_letter:
+                    return False
+                else:
+                    break
+
+    return True
+
+            
