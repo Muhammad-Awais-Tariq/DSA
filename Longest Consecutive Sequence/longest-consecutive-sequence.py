90e82bf675b9940b6d69147fb94034a53c80cd26
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-28 11:47:46 +0500

    added my solution

--- Longest Consecutive Sequence/longest-consecutive-sequence.py
+++ Longest Consecutive Sequence/longest-consecutive-sequence.py
@@ -8,3 +8,26 @@ def longestConsecutive(nums: list) -> int:
     Return:
         int: The len of the longest consective sequence.
     """
+
+    consective_sequences = []
+    sorted_nums = sorted(nums)
+    current_sequence = 1
+
+    if not nums:
+        return 0
+    
+    for num in range(len(sorted_nums) - 1):
+
+        if sorted_nums[num] == sorted_nums[num + 1]:
+            continue
+
+        if sorted_nums[num] + 1 == sorted_nums[num + 1]:
+            current_sequence += 1
+
+        else:
+            consective_sequences.append(current_sequence)
+            current_sequence = 1
+
+    consective_sequences.append(current_sequence)
+
+    return max(consective_sequences)
