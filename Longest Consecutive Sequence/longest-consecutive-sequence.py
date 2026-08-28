dd033a4504346f612cf9459ad97150a9c32cac75
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-28 11:32:16 +0500

    added the docstring

---
+++ Longest Consecutive Sequence/longest-consecutive-sequence.py
@@ -0,0 +1,10 @@
+
+def longestConsecutive(nums: list) -> int:
+    """Return the length of the longest consective sequence of number in the given list.
+
+    Parameter:
+        nums(list): The list in which we want to find the longest seqeunce.
+    
+    Return:
+        int: The len of the longest consective sequence.
+    """
