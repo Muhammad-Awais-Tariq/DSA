c400e095684cab2a9c6839c1b7a4ec66618e3ba9
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-28 16:28:57 +0500

    solved: longest consecutive sequence  [Hashset]

--- Longest Consecutive Sequence/longest-consecutive-sequence.py
+++ Longest Consecutive Sequence/longest-consecutive-sequence.py
@@ -1,4 +1,5 @@
 
+# -------------------------------------- optimal Solution -------------------------------------------------------
 def longestConsecutive(nums: list) -> int:
     """Return the length of the longest consective sequence of number in the given list.
 
@@ -9,25 +10,58 @@ def longestConsecutive(nums: list) -> int:
         int: The len of the longest consective sequence.
     """
 
-    consective_sequences = []
-    sorted_nums = sorted(nums)
-    current_sequence = 1
-
     if not nums:
         return 0
     
-    for num in range(len(sorted_nums) - 1):
+    nums_set = set(nums)
+    longest_sequence = 0
+    
+    for num in nums_set:
 
-        if sorted_nums[num] == sorted_nums[num + 1]:
+        if num - 1 in nums_set:
             continue
 
-        if sorted_nums[num] + 1 == sorted_nums[num + 1]:
-            current_sequence += 1
+        current_seq = 1
+        current_num = num
+        while current_num + 1 in nums_set:
+            current_num += 1
+            current_seq += 1
 
-        else:
-            consective_sequences.append(current_sequence)
-            current_sequence = 1
+        longest_sequence = max(current_seq , longest_sequence)
 
-    consective_sequences.append(current_sequence)
+    return longest_sequence
 
-    return max(consective_sequences)
+
+# -------------------------------------- My solution not optimal -------------------------------------------------------
+# def longestConsecutive(nums: list) -> int:
+#     """Return the length of the longest consective sequence of number in the given list.
+
+#     Parameter:
+#         nums(list): The list in which we want to find the longest seqeunce.
+    
+#     Return:
+#         int: The len of the longest consective sequence.
+#     """
+
+#     consective_sequences = []
+#     sorted_nums = sorted(nums)
+#     current_sequence = 1
+
+#     if not nums:
+#         return 0
+    
+#     for num in range(len(sorted_nums) - 1):
+
+#         if sorted_nums[num] == sorted_nums[num + 1]:
+#             continue
+
+#         if sorted_nums[num] + 1 == sorted_nums[num + 1]:
+#             current_sequence += 1
+
+#         else:
+#             consective_sequences.append(current_sequence)
+#             current_sequence = 1
+
+#     consective_sequences.append(current_sequence)
+
+#     return max(consective_sequences)
