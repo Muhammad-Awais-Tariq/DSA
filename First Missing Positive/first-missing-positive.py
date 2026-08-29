db6578281319f3c7a8b52419d06cf93f77d05816
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-29 15:47:53 +0500

    solved: first-missing-positive [Loop]

--- First Missing Positive/first-missing-positive.py
+++ First Missing Positive/first-missing-positive.py
@@ -1,4 +1,49 @@
 
+# ---------------- Optimal Solution ---------------
+def firstMissingPositive(nums: list) -> int:
+    """Returns the first positive missing number in the list.
+
+    Paramters:
+        nums (list): The list of nums we want to check.
+    
+    Return:
+        int: The first positive missing number in the list.
+    """
+
+    n = len(nums)
+    contains = 0
+
+    for i in range(n):
+        if nums[i] == 1:
+            contains += 1
+            break
+
+    if contains == 0:
+        return 1
+    
+    for i in range(n):
+        if nums[i] <= 0 or nums[i] > n:
+            nums[i] = 1
+
+    for i in range(n):
+        a = abs(nums[i])
+        if a == n:
+            nums[0] = -abs(nums[0])
+        else:
+            nums[a] = -abs(nums[a])
+
+    for i in range(1, n):
+        if nums[i] > 0:
+            return i
+        
+    if nums[0] > 0:
+        return n
+    
+    return n + 1
+
+
+
+
 # ----------------- My solution --------------------
 def firstMissingPositive(nums: list) -> int:
     """Returns the first positive missing number in the list.
