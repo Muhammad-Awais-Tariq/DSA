7c4cfb1bed7f108221c7c4afcfde59911da16401
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-29 13:15:35 +0500

    added the doc string

---
+++ First Missing Positive/first-missing-positive.py
@@ -0,0 +1,10 @@
+
+def firstMissingPositive(nums: list) -> int:
+    """Returns the first positive missing number in the list.
+
+    Paramters:
+        nums (list): The list of nums we want to check.
+    
+    Return:
+        int: The first positive missing number in the list.
+    """
