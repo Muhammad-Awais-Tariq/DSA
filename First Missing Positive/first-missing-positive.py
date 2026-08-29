70bca4898f6549b8e2b3bfb0c634ae8c1008356f
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-08-29 13:34:36 +0500

    added my solution

--- First Missing Positive/first-missing-positive.py
+++ First Missing Positive/first-missing-positive.py
@@ -1,4 +1,5 @@
 
+# ----------------- My solution --------------------
 def firstMissingPositive(nums: list) -> int:
     """Returns the first positive missing number in the list.
 
@@ -8,3 +9,17 @@ def firstMissingPositive(nums: list) -> int:
     Return:
         int: The first positive missing number in the list.
     """
+
+    sorted_num = sorted(nums)
+    expected = 1
+
+    for num in sorted_num:
+        if num == expected:
+            expected += 1
+        elif num > expected:
+            break
+
+    return expected
+
+print(firstMissingPositive([7,8,9,11,12]))
+
