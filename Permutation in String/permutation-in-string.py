abe4f6af2754c8eb66f35f8570924d8411b3945b
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-20 19:35:43 +0500

    added my solution

---
+++ Permutation in String/permutation-in-string.py
@@ -0,0 +1,21 @@
+
+
+
+
+# --------------------------- My solution --------------------------------------------------
+
+# def checkInclusion(self, s1, s2):
+#     """
+#     :type s1: str
+#     :type s2: str
+#     :rtype: bool
+#     """
+    
+#     s1 = sorted(s1)
+#     len_1 = len(s1)
+#     for i in range(len(s2) - len_1 + 1):
+#         current_slide = s2[i:i + len_1]
+#         if s1 == sorted(current_slide):
+#             return True
+    
+#     return False
