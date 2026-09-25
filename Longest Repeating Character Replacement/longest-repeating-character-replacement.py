021b4ed184e8eb341b11bdc5f6ef7e263e5f8d01
Muhammad-Awais-Tariq <muhammad.awais.tariq.dev@gmail.com>
2026-09-25 11:15:47 +0500

    solved: longest Repeating Character Replacement [Sliding Window]

--- Longest Repeating Character Replacement/longest-repeating-character-replacement.py
+++ Longest Repeating Character Replacement/longest-repeating-character-replacement.py
@@ -5,4 +5,17 @@ def characterReplacement(self, s, k):
     :rtype: int
     """
 
-    
+    all_chars = [0] * 26
+    left = max_len = max_occurance = 0
+
+    for right in range(len(s)):
+        all_chars[ord(s[right]) - ord("A")] += 1
+        max_occurance = max(max_occurance , all_chars[ord(s[right]) - ord("A")])
+
+        if (right - left + 1 ) - max_occurance > k:
+            all_chars[ord(s[left]) - ord("A")] -= 1
+            left += 1
+
+        max_len = max(max_len , right - left + 1)
+
+    return max_len
